8b3b005fd910c824b2c029953e24353c6b149ba6
leVHENIN <levhenin@gmail.com>
2026-01-16 10:04:43 +0100

    Fix className for span in Header component
    Text-size and line-height together for Name of the app's description

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -49,7 +49,7 @@ const Header = ({ onMenuToggle, username = "heninvu" }: HeaderProps) => {
             <p className="text-xs opacity-80">Adapted Advanced Documentation</p>
           </div>
           <span className="sm:hidden text-lg font-bold font-serif">
-            Aadoc <span className="text-xs font-normal opacity-80"><br/>Adapted Advanced Documentation</span>
+            Aadoc <span className="text-xs/6 font-normal opacity-80"><br/>Adapted Advanced Documentation</span>
           </span>
         </div>
 
